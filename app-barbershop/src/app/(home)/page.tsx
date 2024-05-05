2dce52539b99d98c1c9ca0f656cbf3fda41d52ba
KeuvynDEV <keuvyntdev@gmail.com>
2024-05-05 17:16:50 -0300

    Feat: Add initial booking component

--- app-barbershop/src/app/(home)/page.tsx
+++ app-barbershop/src/app/(home)/page.tsx
@@ -3,12 +3,16 @@ import Header from "../_components/header";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale/pt-BR";
 import Search from "./_components/search";
+import BookingItem from "../_components/booking-item";
 
 export default function Home() {
   return (
     <div >
+
+      {/* COMPONENTE GLOBAL HEADER */}
       <Header />
 
+      {/* WELCOME */}
       <div className="px-5 pt-5">
         <h2 className="text-xl font-bold">Olá, Miguel!</h2>
         <p className="capitalize text-sm">
@@ -17,9 +21,17 @@ export default function Home() {
         })}</p>
       </div>
         
-      <div className="px-5 mt-6">
+      {/* COMPONENTE LOCAL SEARCH */}
+      <div className="px-5 mt-6 ">
         <Search />
       </div>
+
+      {/* COMPONENTE GLOBAL BOOKING */}
+      <div className="px-5 mt-6">
+        <h2 className="text-xs uppercase text-gray-400 font-bold mb-3">AGENDAMENTOS</h2>
+        <BookingItem />
+      </div>
+
     </div>
   );
 }
