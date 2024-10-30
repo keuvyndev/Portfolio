ebdd5d8b7d10907c1a2bae3e10104a44818b5629
KeuvynDEV <keuvyntdev@gmail.com>
2024-10-29 21:46:58 -0300

    feat: add bookings page

--- app-barbershop/src/app/(home)/page.tsx
+++ app-barbershop/src/app/(home)/page.tsx
@@ -35,10 +35,10 @@ export default async function Home() {
       </div>
 
       {/* COMPONENTE GLOBAL BOOKING-ITEM */}
-      <div className="px-5 mt-6">
+      {/* <div className="px-5 mt-6">
         <h2 className="text-xs uppercase text-gray-400 font-bold mb-3">Agendamentos</h2>
         <BookingItem />
-      </div>
+      </div> */}
 
       {/* COMPONENTE LOCAL BARBERSHOP-ITEM */}
       <div className="mt-6">
---
+++ app-barbershop/src/app/bookings/page.tsx
@@ -0,0 +1,58 @@
+import { getServerSession } from "next-auth";
+import Header from "../_components/header";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+import { db } from "../_lib/prisma";
+import BookingItem from "../_components/booking-item";
+import { isFuture } from "date-fns/isFuture";
+import { isPast } from "date-fns/isPast";
+
+const BookingsPage = async () => {
+   
+   // Recuperar sessão do usuário e verificar se ele está logado ou não.
+   const session = await getServerSession(authOptions);
+
+   // Caso o usuário não esteja logado, redireciona para a tela inicial.
+   if(!session?.user){
+       return redirect("/");
+   }
+   
+   const bookings = await db.booking.findMany({
+      where: {
+         userId: (session.user as any).id,
+      },
+      include:{
+         service: true,
+         barbershop: true
+      }
+   })
+
+   const bookingConfirmed = bookings.filter(booking => isFuture(booking.date)); 
+   const bookingFinished = bookings.filter(booking => isPast(booking.date)); 
+
+   return ( 
+      <>
+         <Header />
+      
+         <div className="px-5 py-6">
+            <h1 className="text-xl font-bold">Agendamentos</h1>
+            
+            <h2 className="text-gray-400 uppercase font-bold text-sm my-6 mb-3">Confirmados</h2>
+            <div className="flex flex-col gap-3">
+               {bookingConfirmed.map( (booking) => (
+               <BookingItem key ={booking.id} booking={booking} /> 
+            ) )}
+            </div>
+
+            <h2 className="text-gray-400 uppercase font-bold text-sm my-6 mb-3">Finalizados</h2>
+            <div className="flex flex-col gap-3">
+               {bookingFinished.map( (booking) => (
+               <BookingItem key ={booking.id} booking={booking} /> 
+            ) )}
+            </div>
+         </div>
+      </>
+    );
+}
+ 
+export default BookingsPage;
