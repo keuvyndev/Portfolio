893f1bd5eb41bd2ea38393f0c101a24e4fb35f36
KeuvynDEV <keuvyntdev@gmail.com>
2024-11-05 01:21:48 -0300

    feat: add products list and product item components

---
+++ app-fsw-foods/app/_components/products-list.tsx
@@ -0,0 +1,33 @@
+import { db } from "../_lib/prisma";
+import ProductItem from "./product-item";
+
+const ProductList = async () => {
+
+   const products = await db.product.findMany({
+      where: {
+         discountPercent: {
+            gt:0,
+         }
+      },
+      take: 10,
+      include: {
+         restaurant: {
+            select: {
+               name: true,
+            }
+         }
+      }
+   });
+
+   return ( 
+      <>
+         <div className="flex overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
+            {products.map(product =>
+               <ProductItem key={product.id} product={product} />
+            )}
+         </div>
+     </>
+   );
+}
+ 
+export default ProductList;
--- app-fsw-foods/app/page.tsx
+++ app-fsw-foods/app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./_components/header";
 import Search from "./_components/search";
 import CategoryList from "./_components/category_list";
 import Image from "next/image";
+import ProductList from "./_components/products-list";
+import { Button } from "./_components/ui/button";
+import { ChevronRightIcon } from "lucide-react";
 
 export const metadata: Metadata = {
   title: "Delivery de Comida - FSW Food",
@@ -20,15 +23,28 @@ const Home = () => {
         <CategoryList />
       </div>
 
-      <Image 
-        src="/promo-banner-01.png" 
-        alt="Até 30% de desconto em pizzas!" 
-        height={0} 
-        width={0} 
-        className="w-full h-auto" 
-        sizes="100vw" 
-        quality={100} 
-      />
+      <div className="px-5 pt-6">
+        <Image 
+          src="/promo-banner-01.png" 
+          alt="Até 30% de desconto em pizzas!" 
+          height={0} 
+          width={0} 
+          className="w-full h-auto" 
+          sizes="100vw" 
+          quality={100} 
+        />
+      </div>
+
+      <div className="pt-6 space-y-4">
+        <div className="px-5 flex justify-between items-center">
+          <h2 className="font-semibold">Pedidos Recomendados</h2>
+          <Button variant="ghost" className="h-fit p-0 text-primary hover:bg-transparent">
+            Ver todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+        <ProductList />
+      </div>
     </>
   );
 }
