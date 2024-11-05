a6f7ed227bdb9d50c8109334689f4a75ad3f97a2
KeuvynDEV <keuvyntdev@gmail.com>
2024-11-05 11:48:09 -0300

    chore: add prisma and database setup

---
+++ finance-ai/app/subscription/page.tsx
@@ -0,0 +1,7 @@
+const Subscription = () => {
+   return ( 
+     <h1>Esta á minha subscriptionpage.</h1>
+   );
+ }
+  
+ export default Subscription;
---
+++ finance-ai/app/transactions/page.tsx
@@ -0,0 +1,7 @@
+const Transaction = () => {
+   return ( 
+     <h1>Esta á minha transactionpage.</h1>
+   );
+ }
+  
+ export default Transaction;
