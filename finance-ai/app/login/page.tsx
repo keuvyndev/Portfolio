6c81bb658993c03047517d4d8a1059c061549692
KeuvynDEV <keuvyntdev@gmail.com>
2024-11-05 12:52:28 -0300

    feat: add initial login page

--- finance-ai/app/login/page.tsx
+++ finance-ai/app/login/page.tsx
@@ -1,7 +1,32 @@
+import { Metadata } from "next";
+import Image from "next/image";
+import { Button } from "../_components/ui/button";
+import { LogInIcon } from "lucide-react";
+
+export const metadata: Metadata = {
+   title: "Efetue Login - Finance AI",
+}
+
 const LoginPage = () => {
    return (
-      <>
-      </>
+      <div className="grid h-full grid-cols-2">
+         {/* ESQUERDA - LOGIN */}
+         <div className="flex flex-col h-full justify-center p-8 max-w-[550px] mx-auto">
+            <Image src="/logo.svg" alt="Finance AI" width={173} height={39} className="mb-8" />
+            <h1 className="text-4xl font-bold mb-3">Bem-Vindo</h1>
+            <p className="text-muted-foreground mb-8"> A Finance AI é uma plataforma de gestão financeira que utiliza IA
+               para monitorar suas movimentações, oferecer insights personalizados,
+               facilitando o controle do seu orçamento. </p>
+            <Button variant="outline">
+               <LogInIcon className="mr-2" />
+               Fazer Login ou criar conta
+            </Button>
+         </div>
+         {/* DIREITA - IMAGEM */}
+         <div className="relative h-full w-full">
+            <Image src="/login.svg" alt="Faça login" fill className="object-cover" />
+         </div>
+      </div>
    );
 }
 
