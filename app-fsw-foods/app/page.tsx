29b201451a685d3dc64dc7545314a47cce21b35b
KeuvynDEV <keuvyntdev@gmail.com>
2024-11-04 23:29:28 -0300

    feat: add initial search component

--- app-fsw-foods/app/page.tsx
+++ app-fsw-foods/app/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import Header from "./_components/header";
+import Search from "./_components/search";
 
 export const metadata: Metadata ={
   title: "Delivery de Comida - FSW Food",
@@ -7,7 +8,12 @@ export const metadata: Metadata ={
 
 const Home = () => {
   return ( 
+    <>
       <Header />
+      <div className="px-5 py-6">
+        <Search /> 
+      </div>
+    </>
   );
 }
  
--- app-fsw-foods/dev_notes/developing_notes.md
+++ app-fsw-foods/dev_notes/developing_notes.md
@@ -7,4 +7,5 @@
 ```bash
 npx shadcn@latest add card
 npx shadcn@latest add button
+npx shadcn@latest add input
 ```
