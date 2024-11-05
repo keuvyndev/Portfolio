e5277bffa443cde9ab94c330df964755a922cd8a
KeuvynDEV <keuvyntdev@gmail.com>
2024-11-05 00:08:47 -0300

    feat: add promo banner to homepage

--- app-fsw-foods/app/page.tsx
+++ app-fsw-foods/app/page.tsx
@@ -2,24 +2,35 @@ import { Metadata } from "next";
 import Header from "./_components/header";
 import Search from "./_components/search";
 import CategoryList from "./_components/category_list";
+import Image from "next/image";
 
-export const metadata: Metadata ={
+export const metadata: Metadata = {
   title: "Delivery de Comida - FSW Food",
 }
 
 const Home = () => {
-  return ( 
+  return (
     <>
       <Header />
       <div className="px-5 py-6">
-        <Search /> 
+        <Search />
       </div>
 
       <div className="px-5 py-6">
         <CategoryList />
       </div>
+
+      <Image 
+        src="/promo-banner-01.png" 
+        alt="Até 30% de desconto em pizzas!" 
+        height={0} 
+        width={0} 
+        className="w-full h-auto" 
+        sizes="100vw" 
+        quality={100} 
+      />
     </>
   );
 }
- 
+
 export default Home;
