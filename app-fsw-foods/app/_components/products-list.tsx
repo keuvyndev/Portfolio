ee10c3e5195cbdaed8f7272ca290c016e16f03fe
KeuvynDEV <keuvyntdev@gmail.com>
2024-11-07 23:24:26 -0300

    fix: remove async from product list

--- app-fsw-foods/app/_components/products-list.tsx
+++ app-fsw-foods/app/_components/products-list.tsx
@@ -14,17 +14,17 @@ interface ProductListProps {
 }
 
 
-const ProductList = async ({products}:ProductListProps) => {
+const ProductList = ({ products }: ProductListProps) => {
 
-   return ( 
+   return (
       <>
          <div className="flex gap-4 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
             {products.map(product =>
                <ProductItem key={product.id} product={product} />
             )}
          </div>
-     </>
+      </>
    );
 }
- 
+
 export default ProductList;
