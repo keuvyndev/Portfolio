c8d87dfc226dae1f9ca93549b0041e35eeb6caa4
KeuvynDEV <keuvyntdev@gmail.com>
2024-11-11 22:53:47 -0300

    build: rename folder application

--- desktop-finance-ai/app/(home)/_components/ai-report-button.tsx
+++ desktop-finance-ai/app/(home)/_components/ai-report-button.tsx
@@ -89,7 +89,7 @@ const AiReportButton = ({ month, hasPremiumPlan }: AiReportButtonProps) => {
                         </Button>
                      </DialogFooter>
                   </>
-               )};
+               )}
          </DialogContent>
       </Dialog>
    );
