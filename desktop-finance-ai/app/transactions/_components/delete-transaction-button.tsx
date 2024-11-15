7588021ebd0a471746acb86036fd3fb174eaf2c7
KeuvynDEV <keuvyntdev@gmail.com>
2024-11-15 20:58:52 -0300

    feat: add useState for delete operation

--- desktop-finance-ai/app/transactions/_components/delete-transaction-button.tsx
+++ desktop-finance-ai/app/transactions/_components/delete-transaction-button.tsx
@@ -1,8 +1,9 @@
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/app/_components/ui/alert-dialog";
 import { Button } from "@/app/_components/ui/button";
-import { TrashIcon } from "lucide-react";
+import { Loader2Icon, TrashIcon } from "lucide-react";
 import { deleteTransaction } from "../_actions/delete-transaction";
 import { toast } from "sonner";
+import { useState } from "react";
 
 interface DeleteTransactionButtonProps {
    transactionId: string
@@ -10,13 +11,23 @@ interface DeleteTransactionButtonProps {
 
 const DeleteTransactionButton = ({ transactionId }: DeleteTransactionButtonProps) => {
 
+   // Controla o estado de carregamento
+   const [deletIsOperating, setdeletIsOperating] = useState(false)
+
    const handleConfirmDeleteClick = async () => {
+
+      setdeletIsOperating(true);
+
       try {
          await deleteTransaction({ transactionId });
          toast.success("Transação deletada com sucesso!");
+         setdeletIsOperating(false);
       } catch (error) {
          console.error(error);
+         setdeletIsOperating(false);
          toast.error("Ocorreu um erro ao deletar a transação.");
+      } finally {
+         setdeletIsOperating(false);
       }
    }
    return (
@@ -38,7 +49,11 @@ const DeleteTransactionButton = ({ transactionId }: DeleteTransactionButtonProps
                </AlertDialogHeader>
                <AlertDialogFooter>
                   <AlertDialogCancel>Cancelar</AlertDialogCancel>
-                  <AlertDialogAction onClick={handleConfirmDeleteClick}>Continuar</AlertDialogAction>
+                  <AlertDialogAction
+                     onClick={handleConfirmDeleteClick}
+                     disabled={deletIsOperating}>
+                     {deletIsOperating && <Loader2Icon className="animate-spin" />} Continuar
+                  </AlertDialogAction>
                </AlertDialogFooter>
             </AlertDialogContent>
          </AlertDialog>
