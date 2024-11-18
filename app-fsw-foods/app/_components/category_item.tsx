3b97accc1a8eca24acc3f762a78198d6dfeb9232
KeuvynDEV <keuvyntdev@gmail.com>
2024-11-18 01:01:42 -0300

    feat: add category products page

--- app-fsw-foods/app/_components/category_item.tsx
+++ app-fsw-foods/app/_components/category_item.tsx
@@ -1,24 +1,25 @@
 import { Category } from "@prisma/client";
 import Image from "next/image";
+import Link from "next/link";
 
-interface CategoryItemProps{
+interface CategoryItemProps {
    category: Category
 }
 
-const CategoryItem = ({category}: CategoryItemProps) => {
-   return ( 
+const CategoryItem = ({ category }: CategoryItemProps) => {
+   return (
       <>
-         <div className="flex justify-center items-center gap-3 py-3 px-4 bg-white shadow-md rounded-full">
-            <Image 
-               src={category.imageUrl} 
-               alt={category.name} 
-               height={30} 
+         <Link href={`/categories/${category.id}/products`} className="flex justify-center items-center gap-3 py-3 px-4 bg-white shadow-md rounded-full">
+            <Image
+               src={category.imageUrl}
+               alt={category.name}
+               height={30}
                width={30}
             />
             <span className="font-semibold text-sm">{category.name}</span>
-         </div>
+         </Link>
       </>
-    );
+   );
 }
- 
+
 export default CategoryItem;
--- app-fsw-foods/app/_components/product-item.tsx
+++ app-fsw-foods/app/_components/product-item.tsx
@@ -4,6 +4,7 @@ import { calculateProductTotalPrice, formatCurrency } from "../_helpers/price";
 import { ArrowDownIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { cn } from "../_lib/utils";
 
 interface ProductItemProps {
    product: Prisma.ProductGetPayload<{
@@ -15,16 +16,17 @@ interface ProductItemProps {
          }
       }
    }>;
+   className?: string
 }
 
 
-const ProductItem = ({ product }: ProductItemProps) => {
+const ProductItem = ({ product, className }: ProductItemProps) => {
 
    return (
       <>
-         <Link className="w-[150px] min-w-[150px]" href={`/products/${product.id}`}>
+         <Link className={cn("w-[150px] min-w-[150px]", className)} href={`/products/${product.id}`}>
             <div className="w-full space-y-2">
-               <div className="h-[150px] w-full relative">
+               <div className="aspect-square w-full relative ">
                   <Image
                      src={product.imageUrl}
                      alt={product.name}
