8a3d114eb73438b95f414cd9c5a3467c6c56a704
keuvyntdev <keuvyntdev@gmail.com>
2024-12-04 22:04:43 -0300

    feat: move restaurants list for a client component

--- app-fsw-foods/app/_context/cart.tsx
+++ app-fsw-foods/app/_context/cart.tsx
@@ -105,7 +105,9 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
          include: {
             restaurant: {
                select: {
+                  id: true;
                   deliveryFee: true;
+                  deliveryTimeMinutes: true;
                };
             };
          };
--- app-fsw-foods/app/restaurants/page.tsx
+++ app-fsw-foods/app/restaurants/page.tsx
@@ -1,52 +1,15 @@
 "use client"
 
-import { Restaurant } from "@prisma/client";
-import { notFound, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import { searchForRestaurants } from "./_actions/search";
-import Header from "../_components/header";
-import RestaurantItem from "../_components/restaurant-item";
+import { Suspense } from "react";
+import Restaurants from "./_components/restaurants";
 
-const Restaurants = () => {
-
-   const searchParams = useSearchParams();
-   const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
-
-   const searchFor = searchParams.get("search");
-
-   // ?
-   useEffect(() => {
-
-      const fetchRestaurants = async () => {
-
-         if (!searchFor) {
-            return;
-         }
-
-         const foundRestaurants = await searchForRestaurants(searchFor);
-         setRestaurants(foundRestaurants);
-      }
-
-      fetchRestaurants();
-   }, [searchParams])
-
-   if (!searchFor) {
-      return notFound();
-   }
+const RestaurantsPage = () => {
 
    return (
-      <>
-         <Header />
-         <div className="py-6 px-5">
-            <h2 className="mb-6 font-semibold text-lg">Restaurantes Encontrados</h2>
-            <div className="flex flex-col gap-6 w-full">
-               {restaurants.map((restaurant) => (
-                  <RestaurantItem key={restaurant.id} restaurant={restaurant} className="min-w-full max-w-full" />
-               ))}
-            </div>
-         </div>
-      </>
+      <Suspense>
+         <Restaurants />
+      </Suspense>
    );
 }
 
-export default Restaurants;
+export default RestaurantsPage;
