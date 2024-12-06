ca85f786be5f0da83c16f4760fa79347475a1e3a
keuvyntdev <keuvyntdev@gmail.com>
2024-12-05 21:24:59 -0300

    fix: restaurants page

--- app-fsw-foods/app/restaurants/_components/restaurants.tsx
+++ app-fsw-foods/app/restaurants/_components/restaurants.tsx
@@ -1,28 +1,28 @@
 "use client"
 
-import { Restaurant } from "@prisma/client";
+import { Restaurant, UserFavoriteRestaurant } from "@prisma/client";
 import { notFound, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { searchForRestaurants } from "../_actions/search";
 import Header from "@/app/_components/header";
 import RestaurantItem from "@/app/_components/restaurant-item";
 
-const Restaurants = () => {
+interface RestaurantsProps {
+   userFavoritesRestaurants: UserFavoriteRestaurant[];
+}
+
+const Restaurants = ({ userFavoritesRestaurants }: RestaurantsProps) => {
 
    const searchParams = useSearchParams();
    const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
-
    const searchFor = searchParams.get("search");
 
-   // ?
+
    useEffect(() => {
-
       const fetchRestaurants = async () => {
-
          if (!searchFor) {
             return;
          }
-
          const foundRestaurants = await searchForRestaurants(searchFor);
          setRestaurants(foundRestaurants);
       }
@@ -41,7 +41,12 @@ const Restaurants = () => {
             <h2 className="mb-6 font-semibold text-lg">Restaurantes Encontrados</h2>
             <div className="flex flex-col gap-6 w-full">
                {restaurants.map((restaurant) => (
-                  <RestaurantItem key={restaurant.id} restaurant={restaurant} className="min-w-full max-w-full" />
+                  <RestaurantItem
+                     key={restaurant.id}
+                     restaurant={restaurant}
+                     className="min-w-full max-w-full"
+                     userFavoriteRestaurants={userFavoritesRestaurants}
+                  />
                ))}
             </div>
          </div>
--- app-fsw-foods/app/restaurants/recommended/page.tsx
+++ app-fsw-foods/app/restaurants/recommended/page.tsx
@@ -1,10 +1,21 @@
 import Header from "@/app/_components/header";
 import RestaurantItem from "@/app/_components/restaurant-item";
+import { authOptions } from "@/app/_lib/auth";
 import { db } from "@/app/_lib/prisma";
+import { getServerSession } from "next-auth";
 
 const RecommendedRestaurants = async () => {
    const restaurants = await db.restaurant.findMany({
 
+   })
+   const session = await getServerSession(authOptions);
+   const userFavoriteRestaurants = await db.userFavoriteRestaurant.findMany({
+      where: {
+         userId: session?.user?.id,
+      },
+      include: {
+         restaurant: true,
+      }
    })
    return (
       <>
@@ -13,7 +24,12 @@ const RecommendedRestaurants = async () => {
             <h2 className="mb-6 font-semibold text-lg">Restaurantes Recomendados</h2>
             <div className="flex flex-col gap-6 w-full">
                {restaurants.map((restaurant) => (
-                  <RestaurantItem key={restaurant.id} restaurant={restaurant} className="min-w-full max-w-full" />
+                  <RestaurantItem
+                     key={restaurant.id}
+                     restaurant={restaurant}
+                     userFavoriteRestaurants={userFavoriteRestaurants}
+                     className="min-w-full max-w-full"
+                  />
                ))}
             </div>
          </div>
