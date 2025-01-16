bad54b1b0048f4bf9e685edb45c708469e92c064
keuvyntdev <keuvyntdev@gmail.com>
2025-01-16 15:42:57 -0300

    refactor: remove unused projects

--- desktop-tradeCrypto-smart/app/transactions/_actions/delete-transaction/schema.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const deleteTransactionSchema = z.object({
-  transactionId: z.string().uuid(),
-});
-
-export type deleteTransactionSchema = z.infer<typeof deleteTransactionSchema>;
